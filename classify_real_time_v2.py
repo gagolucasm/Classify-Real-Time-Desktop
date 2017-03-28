71dace9edd6d17186a8513a51a64ad97ae89f0ae
Lucas Gago <gagolucasm@hotmail.com>
2017-03-28 15:49:23 -0700

    Minor changes and code comments

--- classify_real_time_v2.py
+++ classify_real_time_v2.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import argparse
 import os.path
 import re
@@ -16,47 +12,38 @@ import time
 from gtts import gTTS
 import pygame
 import os
-FLAGS = None
-
-# pylint: disable=line-too-long
-DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
-# pylint: enable=line-too-long
-
 from threading import Thread
 import cv2
 
+model_dir='/tmp/imagenet'
+DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+
+
+
+
 # Threaded class for performance improvement
-class WebcamVideoStream:
+class VideoStream:
 	def __init__(self, src=0):
-		# initialize the video camera stream and read the first frame
-		# from the stream
 		self.stream = cv2.VideoCapture(src)
 		(self.grabbed, self.frame) = self.stream.read()
- 
-		# initialize the variable used to indicate if the thread should
-		# be stopped
 		self.stopped = False
+		
 	def start(self):
-		# start the thread to read frames from the video stream
 		Thread(target=self.update, args=()).start()
 		return self
  
 	def update(self):
-		# keep looping infinitely until the thread is stopped
-		while True:
-			# if the thread indicator variable is set, stop the thread
+                while True:
 			if self.stopped:
 				return
  
-			# otherwise, read the next frame from the stream
 			(self.grabbed, self.frame) = self.stream.read()
  
 	def read(self):
-		# return the frame most recently read
+                # Return the latest frame
 		return self.frame
  
 	def stop(self):
-		# indicate that the thread should be stopped
 		self.stopped = True
 
 class NodeLookup(object):
@@ -65,10 +52,10 @@ class NodeLookup(object):
                uid_lookup_path=None):
     if not label_lookup_path:
       label_lookup_path = os.path.join(
-          FLAGS.model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
+          model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
     if not uid_lookup_path:
       uid_lookup_path = os.path.join(
-          FLAGS.model_dir, 'imagenet_synset_to_human_label_map.txt')
+          model_dir, 'imagenet_synset_to_human_label_map.txt')
     self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
 
   def load(self, label_lookup_path, uid_lookup_path):
@@ -115,18 +102,18 @@ class NodeLookup(object):
 
 
 def create_graph():
-  """Creates a graph from saved GraphDef file and returns a saver."""
+
   # Creates graph from saved graph_def.pb.
   with tf.gfile.FastGFile(os.path.join(
-      FLAGS.model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
+      model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
 
 
 def maybe_download_and_extract():
-  """Download and extract model tar file."""
-  dest_directory = FLAGS.model_dir
+  #Download and extract model tar file
+  dest_directory = model_dir
   if not os.path.exists(dest_directory):
     os.makedirs(dest_directory)
   filename = DATA_URL.split('/')[-1]
@@ -143,114 +130,93 @@ def maybe_download_and_extract():
   tarfile.open(filepath, 'r:gz').extractall(dest_directory)
 
 
-def main(_):
-  maybe_download_and_extract()
-  create_graph()
-  with tf.Session() as sess:
-    softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-    vs = WebcamVideoStream(src=0).start()
-    sleep(1)
-    frame_count=0
-    score=0
-    start = time.time()
-    pygame.mixer.init()
-    pred=0
-    last=0
-    human_string=None
+# Download and create graph
+maybe_download_and_extract()
+create_graph()
 
-    while True:
-          frame = vs.read()
-          frame_count+=1
-          
-          # Only run every 5 frames
-          if frame_count%5==0:
-              
-              # Save the image as the fist layer of inception is a DecodeJpeg
-              cv2.imwrite("current_frame.jpg",frame)
+# Variables declarations
+frame_count=0
+score=0
+start = time.time()
+pygame.mixer.init()
+pred=0
+last=0
+human_string=None
 
-              image_data = tf.gfile.FastGFile("./current_frame.jpg", 'rb').read()
-              predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0': image_data})
+# Init video stream
+vs = VideoStream(src=0).start()
 
-              predictions = np.squeeze(predictions)
-              node_lookup = NodeLookup()
-              
-              # change flag for more predictions
-              top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
-              for node_id in top_k:
-                human_string_n = node_lookup.id_to_string(node_id)
-                score = predictions[node_id]
-                if score>.5:
-                        # Some manual corrections
-                        if human_string_n=="stethoscope":human_string_n="Headphones"
-                        if human_string_n=="spatula":human_string_n="fork"
-                        if human_string_n=="iPod":human_string_n="iPhone"
-                        human_string=human_string_n
+# Start tensroflow session
+with tf.Session() as sess:
+        softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
 
-                        lst=human_string.split()
-                        human_string=" ".join(lst[0:2])
-                        human_string_filename=str(lst[0])
-                
-                current= time.time()
-                fps=frame_count/(current-start)
-                
-          # Speech module        
-          if last>40 and pygame.mixer.music.get_busy() == False and  human_string==human_string_n:
-              pred+=1
-              name=human_string_filename+".mp3"
-              
-              # Only get from google if we dont have it
-              if not os.path.isfile(name):
-                  tts = gTTS(text="I see a "+human_string, lang='en')
-                  tts.save(name)
-        
-              last=0
-              pygame.mixer.music.load(name)
-              pygame.mixer.music.play()
+        while True:
+                frame = vs.read()
+                frame_count+=1
 
-          # Show info during some time              
-          if last<40 and frame_count>10:
-              cv2.putText(frame,human_string, (20,400), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255))
-              cv2.putText(frame,str(np.round(score,2))+"%", (20,440), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255))
+                # Only run every 5 frames
+                if frame_count%5==0:
 
-          if frame_count>20:cv2.putText(frame,"fps: "+str(np.round(fps,2)), (460,460), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255))
-              
-          cv2.imshow("Frame", frame)
-          last+=1
+                        # Save the image as the fist layer of inception is a DecodeJpeg
+                        cv2.imwrite("current_frame.jpg",frame)
 
+                        image_data = tf.gfile.FastGFile("./current_frame.jpg", 'rb').read()
+                        predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0': image_data})
 
-          # if the 'q' key is pressed, stop the loop
-          if cv2.waitKey(1) & 0xFF == ord("q"):break
+                        predictions = np.squeeze(predictions)
+                        node_lookup = NodeLookup()
 
-  # cleanup everything
-  vs.stop()
-  cv2.destroyAllWindows()
-  from os import listdir
-  from os.path import join
+                        # change n_pred for more predictions
+                        n_pred=1
+                        top_k = predictions.argsort()[-n_pred:][::-1]
+                        for node_id in top_k:
+                                human_string_n = node_lookup.id_to_string(node_id)
+                                score = predictions[node_id]
+                        if score>.5:
+                                # Some manual corrections
+                                if human_string_n=="stethoscope":human_string_n="Headphones"
+                                if human_string_n=="spatula":human_string_n="fork"
+                                if human_string_n=="iPod":human_string_n="iPhone"
+                                human_string=human_string_n
 
-  dir = "/Users/gagol/Desktop/DLHack"
-  test=os.listdir(dir)      
-  sess.close()
-  print("Done")
+                                lst=human_string.split()
+                                human_string=" ".join(lst[0:2])
+                                human_string_filename=str(lst[0])
+
+                        current= time.time()
+                        fps=frame_count/(current-start)
+
+                # Speech module        
+                if last>40 and pygame.mixer.music.get_busy() == False and  human_string==human_string_n:
+                        pred+=1
+                        name=human_string_filename+".mp3"
+
+                        # Only get from google if we dont have it
+                        if not os.path.isfile(name):
+                                tts = gTTS(text="I see a "+human_string, lang='en')
+                                tts.save(name)
+
+                        last=0
+                        pygame.mixer.music.load(name)
+                        pygame.mixer.music.play()
+
+                # Show info during some time              
+                if last<40 and frame_count>10:
+                        cv2.putText(frame,human_string, (20,400), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255))
+                        cv2.putText(frame,str(np.round(score,2))+"%", (20,440), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255))
+
+                if frame_count>20:
+                        cv2.putText(frame,"fps: "+str(np.round(fps,2)), (460,460), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255))
+
+                cv2.imshow("Frame", frame)
+                last+=1
 
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
+                # if the 'q' key is pressed, stop the loop
+                if cv2.waitKey(1) & 0xFF == ord("q"):break
 
-  parser.add_argument(
-      '--model_dir',
-      type=str,
-      default='/tmp/imagenet',
-      help="""\
-      Path to classify_image_graph_def.pb,
-      imagenet_synset_to_human_label_map.txt, and
-      imagenet_2012_challenge_label_map_proto.pbtxt.\
-      """
-  )
-  parser.add_argument(
-      '--num_top_predictions',
-      type=int,
-      default=1,
-      help='Display this many predictions.'
-  )
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+# cleanup everything
+vs.stop()
+cv2.destroyAllWindows()     
+sess.close()
+print("Done")
